ad306efae9a0e728acd6b605fdfedf396d21f649
Sourabh Gond <137901668+Sourabh9879@users.noreply.github.com>
2023-09-20 15:33:56 +0530

    Add files via upload

---
+++ Assignment-1/java1.java
@@ -0,0 +1,6 @@
+//Write a program to print “Welcome to Java Programming”.
+public class java1{
+    public static void main(String[] args){
+        System.out.println("Welcome to Java Programming");
+    }
+}
